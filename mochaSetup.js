4da034eb6aefef0798d4d13b894c5c90f8cdaa3f
Anton Zabelin <azabelin@t1-consulting.ru>
2024-07-01 16:11:38 +0300

    upd mochasetup 004

--- mochaSetup.js
+++ mochaSetup.js
@@ -1,8 +1,4 @@
 import { JSDOM } from 'jsdom';
-import { register } from 'node:module';
-import { pathToFileURL } from 'node:url';
-
-register('ts-node/esm', pathToFileURL('./'));
 
 const jsdom = new JSDOM('<body><div id="app"></div></body>', { url: 'https://example.org' });
 
